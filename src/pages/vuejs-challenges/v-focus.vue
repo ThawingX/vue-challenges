ca0504297fee499d55d6e8755cc303bf52a5e4fb
Dylan Long <thawingx@gmail.com>
2024-05-16 00:49:44 +0800

    feat: directive v-focus

---
+++ src/pages/vuejs-challenges/v-focus.vue
@@ -0,0 +1,29 @@
+<script setup lang='ts'>
+import { ref } from "vue"
+
+const state = ref(false)
+
+/**
+ * Implement the custom directive
+ * Make sure the input element focuses/blurs when the 'state' is toggled
+ *
+*/
+const VFocus = {
+  updated: (el, binding) => {
+    if (binding.value) {
+      el.focus()
+    } else {
+      el.blur()
+    }
+  }
+}
+
+setInterval(() => {
+  state.value = !state.value
+}, 2000)
+
+</script>
+
+<template>
+  <input v-focus="state" type="text">
+</template>
